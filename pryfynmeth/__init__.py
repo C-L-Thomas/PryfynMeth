03fcc1f378280a0d32bcf48e2103c96532ee06bd
Christian Thomas <Christianluthomas@gmail.com>
2025-08-28 18:03:10 +0100

    Update __init__.py

--- pryfynmeth/__init__.py
+++ pryfynmeth/__init__.py
@@ -4,10 +4,12 @@ from . import (
     binomial,
     bisulphite_preprocessing,
     condition_cpg_mean,
+    cpg_motif_enrichment,
     cov_convert,
     dss_prepare,
     filter,
     nanopore_adjust,
+    anopore_preprocessing,
     sample_combine,
     statistics
 )
